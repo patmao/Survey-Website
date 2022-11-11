135505841f22a95f598b2bea079ade5e15013114
awarena <76248192+awarena@users.noreply.github.com>
2022-11-11 13:13:14 -0500

    change database URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports =
 {
     //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://user1:U5QIUOXhqPWrId1N@cluster1.cojokwi.mongodb.net/business_contacts?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://user1:U5QIUOXhqPWrId1N@cluster1.cojokwi.mongodb.net/group_project_group1?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
