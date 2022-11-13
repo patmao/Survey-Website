3f2bfa5fcfb7e7bd09056afeee58874e6c4c96d6
Song Malisa Se <sesongmaliisa@gmail.com>
2022-11-13 16:41:54 -0500

    change to textarea for now and update delete button

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -10,6 +10,19 @@ Date: 10/23/2022
 
     function Start() {
         console.log("App Started...");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger')
+
+        for(button of deleteButtons)
+        {
+            button.addEventListener('click', (event)=> {
+                if(!confirm("Are you sure you want to delete?"))
+                {
+                    event.preventDefault();
+                    window.location.assign('/surveys');
+                }
+            })
+        }
     }
 
     window.addEventListener("load", Start);
